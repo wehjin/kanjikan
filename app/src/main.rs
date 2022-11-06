22982bf2ef3b1988cdd119f67e9c72515eee39c7
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2022-11-05 17:24:16 -0700

    Draws aframe basic example.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,46 +1,30 @@
+extern crate gloo_utils;
+
+use web_sys::console;
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
+const SCENE_INNER: &str = include_str!("a_scene_inner.html");
 
-struct Model {
-    value: i64,
-}
+enum Msg {}
+
+struct Model {}
 
 impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
+	type Message = Msg;
+	type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            value: 0,
-        }
-    }
+	fn create(_ctx: &Context<Self>) -> Self {
+		Self {}
+	}
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
-    }
+	fn view(&self, _ctx: &Context<Self>) -> Html {
+		let div = gloo_utils::document().create_element("a-scene").unwrap();
+		div.set_inner_html(SCENE_INNER);
+		console::log_1(&div);
+		Html::VRef(div.into())
+	}
 }
 
 fn main() {
-    yew::start_app::<Model>();
+	yew::start_app::<Model>();
 }
