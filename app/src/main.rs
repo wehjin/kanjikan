2e2a3f966a4b2ae4c503b069804a61460e5d9057
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2022-11-07 14:34:38 -0800

    Adds laser-controls component.

--- app/src/main.rs
+++ app/src/main.rs
@@ -21,12 +21,14 @@ impl Component for Model {
 		//<a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
 		let x = html! {
 			<a-scene>
+				<a-entity laser-controls="hand: left" raycaster="objects: .collidable; far: 10"></a-entity>
+				<a-entity laser-controls="hand: right" raycaster="objects: .collidable;"></a-entity>
 				<a-sky src="https://cdn.glitch.global/78e9163c-0114-44ea-b6d2-3fb627944853/sky360.jpeg?v=1667676752316"></a-sky>
-				<a-plane position="0 0 -4" rotation="-90 0 0" width="8" height="4" color="#7BC8A4"></a-plane>
+				<a-plane class="collidable" position="0 0 -4" rotation="-90 0 0" width="8" height="4" color="#7BC8A4"></a-plane>
 				<a-sphere position="0 0 -2" radius="0.05" color="#333333"></a-sphere>
-				<a-entity position="-2 0.5 -3" rotation="0 45 0" geometry="primitive: box" material="color: #4CC3D9"/>
-				<a-sphere position="0 0.95 -6" radius="1" color="#EF2D5E"></a-sphere>
-				<a-cylinder position="2 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
+				<a-entity class="collidable" position="-2 0.5 -3" rotation="0 45 0" geometry="primitive: box" material="color: #4CC3D9"/>
+				<a-sphere class="collidable" position="0 0.95 -6" radius="1" color="#EF2D5E"></a-sphere>
+				<a-cylinder class="collidable" position="2 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder>
 				<KanjiBox text="見学" position={Xyz::new(0.0, 0.5, -3.5)}/>
 				<KanjiBox text="すべて" position={Xyz::new(-2.0, 1.5, -3.0)} rotation={Xyz::new(0.0, 45.0, 0.0)}/>
 			</a-scene>
@@ -86,7 +88,7 @@ pub fn kanji_block(props: &KanjiBoxProps) -> Html {
 	let html = html! {
 		<a-entity position={props.position} rotation={props.rotation}>
 			<a-text value={ text_value } position="0 0 0" side="double" color="black" align="center" shader="msdf" font="https://cdn.glitch.global/78e9163c-0114-44ea-b6d2-3fb627944853/NotoSansJP-Black.json?v=1667671610240" font-image="https://cdn.glitch.global/78e9163c-0114-44ea-b6d2-3fb627944853/NotoSansJP-Black.png?v=1667673945948"></a-text>
-			<a-entity position="0 0 0" geometry="primitive: box; width: 1.0; height: 1.0" material="color: #445566; opacity: 0.4; transparent: true"/>
+			<a-entity class="collidable" position="0 0 0" geometry="primitive: box; width: 1.0; height: 1.0" material="color: #445566; opacity: 0.4; transparent: true"/>
 		</a-entity>
 	};
 	html
